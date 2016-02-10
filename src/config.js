6132c5c336edd3b7e7096519a4ccb17206341f3a
Aliaksei Shytkin <e79eas@gmail.com>
2016-02-10 17:17:14 +0300

    Fix config

--- src/config.js
+++ src/config.js
@@ -4,21 +4,21 @@ tau.mashups.addModule('EmbeddedPages/config', {
         /* Sample embedded page of the 'CustomPageUrl' Custom Field of a User Story of a Project with the 'Scrum' Process */
         entityTypeName: 'UserStory',
         customFieldName: 'Image of the day',
-        processName: 'Kanban',
+        processName: 'Scrum',
         frameTemplate: '<iframe src="${url}" width="854" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
     }, {
         /* Sample embedded page of the 'CustomPageUrl' Custom Field of a Bug of any Project */
         entityTypeName: 'UserStory',
         customFieldName: 'CustomPageUrl'
-    // }, {
-    //      Sample embedded page of the 'CustomEmbeddedVideoUrl' Custom Field with the overridden template of the tab frame
-    //     entityTypeName: 'Project',
-    //     customFieldName: 'CustomEmbeddedVideoUrl',
-    //     frameTemplate: '<iframe src="${url}" width="854" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
-    // }, {
-    //     /* Embedded page for entity without process */
-    //     entityTypeName: 'User',
-    //     customFieldName: 'Facebook Profile'
+    }, {
+        /* Sample embedded page of the 'CustomEmbeddedVideoUrl' Custom Field with the overridden template of the tab frame */
+        entityTypeName: 'Project',
+        customFieldName: 'CustomEmbeddedVideoUrl',
+        frameTemplate: '<iframe src="${url}" width="854" height="480" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>'
+    }, {
+        /* Embedded page for entity without process */
+        entityTypeName: 'User',
+        customFieldName: 'Facebook Profile'
     }]
 
 });
